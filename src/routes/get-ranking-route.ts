e752675eed92707029cc6b8edc4c6b257922feef
MateusAugustoo <mateusaugusto2017pp@gmail.com>
2025-02-20 22:49:45 -0300

    add getRankingRoute to retrieve and return subscriber rankings

---
+++ src/routes/get-ranking-route.ts
@@ -0,0 +1,32 @@
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+import { env } from '../env'
+import { accessInviteLink } from '../functions/access-invite-link'
+import { getRanking } from '../functions/get-ranking'
+
+export const getRankingRoute: FastifyPluginAsyncZod = async app => {
+  app.get(
+    '/ranking',
+    {
+      schema: {
+        tags: ['referral'],
+        response: {
+          200: z.object({
+            ranking: z.array(
+              z.object({
+                id: z.string(),
+                name: z.string(),
+                score: z.number(),
+              })
+            ),
+          }),
+        },
+      },
+    },
+    async () => {
+      const { rankingWithScore } = await getRanking()
+
+      return { ranking: rankingWithScore }
+    }
+  )
+}
