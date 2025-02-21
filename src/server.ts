8f099daca7ade78a93e0ef81528b29b6c4b475a1
MateusAugustoo <mateusaugusto2017pp@gmail.com>
2025-02-20 22:48:48 -0300

    add routes for subscriber invite tracking and ranking functionalities

--- src/server.ts
+++ src/server.ts
@@ -10,6 +10,11 @@ import { fastifySwagger } from '@fastify/swagger'
 import { fastifySwaggerUi } from '@fastify/swagger-ui'
 import { subscribeToEventRoute } from "./routes/subscribe-to-event-route";
 import { env } from "./env";
+import { accessInviteLinkRoute } from "./routes/acces-invite-link-route";
+import { getSubscriberInviteClicksRoute } from "./routes/get-subscriber-invite-clicks-route";
+import { getSubscriberInvitesCountRoute } from "./routes/get-subscriber-invites-count-route";
+import { getSubscriberRankingPositionRoute } from "./routes/get-subscriber-ranking-position-route";
+import { getRankingRoute } from "./routes/get-ranking-route";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -35,6 +40,11 @@ app.register(fastifySwaggerUi, {
 })
 
 app.register(subscribeToEventRoute)
+app.register(accessInviteLinkRoute)
+app.register(getSubscriberInviteClicksRoute)
+app.register(getSubscriberInvitesCountRoute)
+app.register(getSubscriberRankingPositionRoute)
+app.register(getRankingRoute)
 
 app
   .listen({ port: env.PORT })
